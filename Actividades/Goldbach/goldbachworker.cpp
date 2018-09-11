ffecdec85632699e5a3725a70b617a9c2952710c
Lucia Elizondo Sancho <b72694@ecci.ucr.ac.cr@ECCI-LBL-104-10.ecci.ucr.ac.cr>
2018-09-10 19:32:37 -0600

    Optimized isPrime: Mersenne numbers with sqrt

--- Actividades/Goldbach/goldbachworker.cpp
+++ Actividades/Goldbach/goldbachworker.cpp
@@ -110,31 +110,18 @@ long long GoldbachWorker::calculateOddGoldbach(long long number)
 
 bool GoldbachWorker::isPrime(long long n)
 {
-    // Corner cases
-    if (n <= 1)  return false;
-    if (n <= 3)  return true;
-
-    // This is checked so that we can skip
-    // middle five numbers in below loop
-    if (n%2 == 0 || n%3 == 0) return false;
-
-    for (int i=5; i*i<=n; i=i+6)
-        if (n%i == 0 || n%(i+2) == 0)
-           return false;
-
-    return true;    // Corner cases
-    if (n <= 1)  return false;
-    if (n <= 3)  return true;
-
-    // This is checked so that we can skip
-    // middle five numbers in below loop
-    if (n%2 == 0 || n%3 == 0) return false;
-
-    for (int i=5; i*i<=n; i=i+6)
-        if (n%i == 0 || n%(i+2) == 0)
-           return false;
-
-    return true;
+    if (n%2==0) return n==2;
+    if (n%3==0) return n==3;
+    int d=5;
+    long long end = qSqrt(n);
+    while(d<=end)
+    {
+        if(n%d==0) return 0;
+        d+=2;
+        if(n%d==0) return 0;
+        d+=4;
+    }
+    return 1;
 }
 
 long long GoldbachWorker::initialRange()
