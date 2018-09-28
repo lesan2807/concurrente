5d6e1e247549a1535efb4b1245e04de43723f385
Lucia Elizondo Sancho <b72694@ecci.ucr.ac.cr@ECCI-LBL-104-17.ecci.ucr.ac.cr>
2018-09-28 11:52:15 -0600

    Now has n threads with struct

---
+++ Actividades/2-memcompartida/pthreads/hello_i_of_w/hello_w_i.c
@@ -0,0 +1,40 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct
+{
+  size_t number;
+  size_t count;
+}thread_info_t;
+
+void* hello(void* data)
+{
+    thread_info_t* my_info = (thread_info_t*)data;
+    printf("Hello world from thread %zu of %zu\n", my_info->count, my_info->number);
+    return NULL;
+}
+
+int main(int argc, char* argv[])
+{
+  size_t number_threads = sysconf( _SC_NPROCESSORS_ONLN);
+  if(argc > 1 )
+    number_threads = strtoull(argv[1], NULL, 10);
+  pthread_t threads[number_threads];
+  thread_info_t* info = calloc(number_threads, sizeof(thread_info_t));
+      for(size_t index = 0; index < number_threads; ++index)
+      {
+        info[index].number = number_threads;
+        info[index].count = index;
+        pthread_create(&threads[index], NULL, hello, (void*)&info[index]);
+      }
+
+    printf("Hello from main thread\n");
+
+    for(size_t index = 0; index < number_threads; ++index)
+      pthread_join(threads[index], NULL);
+
+    free(info);
+    return 0;
+}
