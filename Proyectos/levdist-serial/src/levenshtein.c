109e46272e7801f95b099ed58d1a3f1bda5adfb1
Lucia Elizondo Sancho <b72694@ecci.ucr.ac.cr@ECCI-LBL-104-05.ecci.ucr.ac.cr>
2018-10-01 18:50:56 -0600

    Changed levenshtein algorithm

--- Proyectos/levdist-serial/src/levenshtein.c
+++ Proyectos/levdist-serial/src/levenshtein.c
@@ -11,63 +11,106 @@
 // Choose the minimum of three numbers
 #define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
 
-size_t levenshtein_ascii(const char* source, const char* target)
-{
-    size_t x, y, source_length, target_length;
-    source_length = strlen(source);
+//size_t levenshtein_ascii(const char* source, const char* target)
+//{
+//    size_t x, y, source_length, target_length;
+//    source_length = strlen(source);
+//    target_length = strlen(target);
+
+//    // size_t matrix[target_length+1][source_length+1];
+//    long** matrix = (long**)calloc(target_length+1, sizeof(long *));
+//    for(size_t index = 0; index < target_length+1; ++index)
+//        matrix[index] = (long*)calloc(source_length+1, sizeof(long));
+
+//    matrix[0][0] = 0;
+//    for (x = 1; x <= target_length; ++x)
+//    {
+//        matrix[x][0] = matrix[x-1][0] + 1;
+//    }
+//    for (y = 1; y <= source_length; ++y)
+//        matrix[0][y] = matrix[0][y-1] + 1;
+//    for (x = 1; x <= target_length; ++x)
+//        for (y = 1; y <= source_length; ++y)
+//            matrix[x][y] = MIN3(matrix[x-1][y] + 1, matrix[x][y-1] + 1, matrix[x-1][y-1] + (source[y-1] == target[x-1] ? 0 : 1));
+
+//    size_t distance = matrix[target_length][source_length];
+//    for ( size_t row_index = 0; row_index < target_length+1; ++row_index )
+//         free(matrix[row_index]);
+//    free(matrix);
+//    return distance;
+//}
+
+#define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
+
+size_t levenshtein_ascii(const char* source, const char* target) {
+    size_t source_length, target_length, x, y, lastdiag, olddiag;
+    source_length = strlen(source);|
     target_length = strlen(target);
-
-    // size_t matrix[target_length+1][source_length+1];
-    long** matrix = (long**)calloc(target_length+1, sizeof(long *));
-    for(size_t index = 0; index < target_length+1; ++index)
-        matrix[index] = (long*)calloc(source_length+1, sizeof(long));
-
-    matrix[0][0] = 0;
-    for (x = 1; x <= target_length; ++x)
-    {
-        matrix[x][0] = matrix[x-1][0] + 1;
-    }
+    size_t* column = malloc((source_length+1)*sizeof(size_t));
     for (y = 1; y <= source_length; ++y)
-        matrix[0][y] = matrix[0][y-1] + 1;
-    for (x = 1; x <= target_length; ++x)
-        for (y = 1; y <= source_length; ++y)
-            matrix[x][y] = MIN3(matrix[x-1][y] + 1, matrix[x][y-1] + 1, matrix[x-1][y-1] + (source[y-1] == target[x-1] ? 0 : 1));
-
-    size_t distance = matrix[target_length][source_length];
-    for ( size_t row_index = 0; row_index < target_length+1; ++row_index )
-         free(matrix[row_index]);
-    free(matrix);
+        column[y] = y;
+    for (x = 1; x <= target_length; ++x) {
+        column[0] = x;
+        for (y = 1, lastdiag = x-1; y <= source_length; ++y) {
+            olddiag = column[y];
+            column[y] = MIN3(column[y] + 1, column[y-1] + 1, lastdiag + (source[y-1] == target[x-1] ? 0 : 1));
+            lastdiag = olddiag;
+        }
+    }
+    size_t distance = column[source_length];
+    free(column);
     return distance;
 }
 
-size_t levenshtein_unicode(const wchar_t* source, const wchar_t* target)
-{
-    size_t x, y, source_length, target_length;
-    source_length = wcslen(source);
+//size_t levenshtein_unicode(const wchar_t* source, const wchar_t* target)
+//{
+//    size_t x, y, source_length, target_length;
+//    source_length = wcslen(source);
+//    target_length = wcslen(target);
+
+//    // size_t matrix[target_length+1][source_length+1];
+//    long** matrix = (long**)calloc(target_length+1, sizeof(long *));
+//    for(size_t index = 0; index < target_length+1; ++index)
+//        matrix[index] = (long*)calloc(source_length+1, sizeof(long));
+
+//    matrix[0][0] = 0;
+//    for (x = 1; x <= target_length; ++x)
+//    {
+//        matrix[x][0] = matrix[x-1][0] + 1;
+//    }
+//    for (y = 1; y <= source_length; ++y)
+//        matrix[0][y] = matrix[0][y-1] + 1;
+//    for (x = 1; x <= target_length; ++x)
+//        for (y = 1; y <= source_length; ++y)
+//            matrix[x][y] = MIN3(matrix[x-1][y] + 1, matrix[x][y-1] + 1, matrix[x-1][y-1] + (source[y-1] == target[x-1] ? 0 : 1));
+
+//    size_t distance = matrix[target_length][source_length];
+//    for ( size_t row_index = 0; row_index < target_length+1; ++row_index )
+//         free(matrix[row_index]);
+//    free(matrix);
+//    return distance;
+//}
+
+size_t levenshtein_unicode(const wchar_t* source, const wchar_t* target) {
+    size_t source_length, target_length, x, y, lastdiag, olddiag;
+    source_length = wcslen(source);|
     target_length = wcslen(target);
-
-    // size_t matrix[target_length+1][source_length+1];
-    long** matrix = (long**)calloc(target_length+1, sizeof(long *));
-    for(size_t index = 0; index < target_length+1; ++index)
-        matrix[index] = (long*)calloc(source_length+1, sizeof(long));
-
-    matrix[0][0] = 0;
-    for (x = 1; x <= target_length; ++x)
-    {
-        matrix[x][0] = matrix[x-1][0] + 1;
-    }
+    size_t* column = malloc((source_length+1)*sizeof(size_t));
     for (y = 1; y <= source_length; ++y)
-        matrix[0][y] = matrix[0][y-1] + 1;
-    for (x = 1; x <= target_length; ++x)
-        for (y = 1; y <= source_length; ++y)
-            matrix[x][y] = MIN3(matrix[x-1][y] + 1, matrix[x][y-1] + 1, matrix[x-1][y-1] + (source[y-1] == target[x-1] ? 0 : 1));
-
-    size_t distance = matrix[target_length][source_length];
-    for ( size_t row_index = 0; row_index < target_length+1; ++row_index )
-         free(matrix[row_index]);
-    free(matrix);
+        column[y] = y;
+    for (x = 1; x <= target_length; ++x) {
+        column[0] = x;
+        for (y = 1, lastdiag = x-1; y <= source_length; ++y) {
+            olddiag = column[y];
+            column[y] = MIN3(column[y] + 1, column[y-1] + 1, lastdiag + (source[y-1] == target[x-1] ? 0 : 1));
+            lastdiag = olddiag;
+        }
+    }
+    size_t distance = column[source_length];
+    free(column);
     return distance;
 }
+
 
 int lev_dist_calculate_files_ascii(lev_dist_files_t* distances, size_t comparisons)
 {
