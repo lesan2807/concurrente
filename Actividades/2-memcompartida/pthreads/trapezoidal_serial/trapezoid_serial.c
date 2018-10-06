d68b9135c8ce6fb1aeb80ed5a825ba0135981d1c
Lucia <@lesan2807@gmail.com>
2018-10-05 18:21:27 -0600

    Did activity for trapexoidal rule (serial)

---
+++ Actividades/2-memcompartida/pthreads/trapezoidal_serial/trapezoid_serial.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double f(double x)
+{
+    return cos(x)*2*x + x/8;
+}
+
+// https://en.wikipedia.org/wiki/Trapezoidal_rule
+// https://www.geeksforgeeks.org/trapezoidal-rule-for-approximate-value-of-definite-integral/
+double trapezoidal_rule(double a, double b, double n)
+{
+    double delta_x = (b-a)/n;
+    double trapezoid_sum = f(a) + f(a+(n*delta_x));
+    for(size_t index = 0; index <= n; ++index)
+    {
+        trapezoid_sum += 2*f(a+(index*delta_x));
+    }
+    return (delta_x/2)*trapezoid_sum;
+}
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    size_t a = strtoull(argv[1], NULL, 10);
+    size_t b = strtoull(argv[2], NULL, 10);
+    size_t n = strtoull(argv[3], NULL, 10);
+
+    printf("%lf\n", trapezoidal_rule(a, b, n));
+
+    return 0;
+}
