4a04c1f4a1c8c56d7937cfeeb53a03a77173e805
Lucia Elizondo Sancho <b72694@ecci.ucr.ac.cr@ECCI-LBL-104-10.ecci.ucr.ac.cr>
2018-12-10 10:39:02 -0600

    Array reentrante

--- Actividades/2-memcompartida/pthreads/array1/array.c
+++ Actividades/2-memcompartida/pthreads/array1/array.c
@@ -1,96 +1,95 @@
 #include <assert.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 #include "array.h"
 
-array_t* array_create(array_t* array, size_t capacity)
+
+array_t array_create(size_t capacity)
 {
 	assert(capacity);
-    array->array_capacity = capacity;
-    array->array_count = 0;
-    array->array_elements = (void**)malloc( capacity * sizeof(void*) );
-    return array;
+	array_t temp = (array_t)malloc(sizeof(array_t)); 
+	temp->array_capacity = capacity;
+	temp->array_count = 0;
+	temp->array_elements = (void**)malloc( capacity * sizeof(void*) );
+	return temp; 
 }
 
-void array_destroy(array_t* array)
+void array_destroy(array_t array)
 {
-    free(array->array_elements);
+	free(array->array_elements);
+	free(array);
 }
 
-int array_increase_capacity(array_t* array)
+int array_increase_capacity(array_t array)
 {
-    size_t new_capacity = 10 * array->array_capacity;
-    void** new_elements = (void**)realloc( array->array_elements, new_capacity * sizeof(void*) );
-    if ( new_elements == NULL )
-        return -1;
+	size_t new_capacity = 10 * array->array_capacity;
+	void** new_elements = (void**)realloc( array->array_elements, new_capacity * sizeof(void*) );
+	if ( new_elements == NULL )
+		return -1;
 
-    array->array_capacity = new_capacity;
-    array->array_elements = new_elements;
+	array->array_capacity = new_capacity;
+	array->array_elements = new_elements;
 
-    return 0; // Success
+	return 0; // Success
 }
 
-int array_decrease_capacity(array_t* array)
+int array_decrease_capacity(array_t array)
 {
-    size_t new_capacity = array->array_capacity / 10;
-    if ( new_capacity < 10 )
-        return 0;
+	size_t new_capacity = array->array_capacity / 10;
+	if ( new_capacity < 10 )
+		return 0;
 
-    void** new_elements = (void**)realloc( array->array_elements, new_capacity * sizeof(void*) );
-    if ( new_elements == NULL )
-        return -1;
+	void** new_elements = (void**)realloc( array->array_elements, new_capacity * sizeof(void*) );
+	if ( new_elements == NULL )
+		return -1;
 
-    array->array_capacity = new_capacity;
-    array->array_elements = new_elements;
+	array->array_capacity = new_capacity;
+	array->array_elements = new_elements;
 
-    return 0; // Success
+	return 0; // Success
 }
 
-size_t array_get_count(const array_t* array)
+size_t array_get_count(const array_t array)
 {
-    return array->array_count;
+	(void)array;
+	return array->array_count;
 }
 
-void* array_get_element(array_t* array, size_t index)
+void* array_get_element(array_t array, size_t index)
 {
-
-    printf("hellow\n");
-    assert( index < array_get_count(array) );
-    return array->array_elements[index];
+	assert( index < array_get_count(array) );
+	return array->array_elements[index];
 }
 
-int array_append(array_t* array, void* element)
+int array_append(array_t array, void* element)
 {
+	if ( array->array_count == array->array_capacity )
+		if ( ! array_increase_capacity(array) )
+			return -1;
 
-    if ( array->array_count == array->array_capacity )
-        if ( ! array_increase_capacity(array) )
-            return -1;
-
-    array->array_elements[array->array_count] = element;
-    ++array->array_count;
-    return 0; // Success
+	array->array_elements[array->array_count++] = element;
+	return 0; // Success
 }
 
-size_t array_find_first(const array_t* array, const void* element, size_t start_pos)
+size_t array_find_first(const array_t array, const void* element, size_t start_pos)
 {
-    for ( size_t index = start_pos; index < array->array_count; ++index )
-        if ( array->array_elements[index] == element )
-            return index;
+	for ( size_t index = start_pos; index < array->array_count; ++index )
+		if ( array->array_elements[index] == element )
+			return index;
 
-    return array_not_found;
+	return array_not_found;
 }
 
-int array_remove_first(array_t* array, const void* element, size_t start_pos)
+int array_remove_first(array_t array, const void* element, size_t start_pos)
 {
-    size_t index = array_find_first(array, element, start_pos);
-    if ( index == array_not_found )
-        return -1;
+	size_t index = array_find_first(array, element, start_pos);
+	if ( index == array_not_found )
+		return -1;
 
-    for ( --array->array_count; index < array->array_count; ++index )
-        array->array_elements[index] = array->array_elements[index + 1];
-    if ( array->array_count == array->array_capacity / 10 )
-        array_decrease_capacity(array);
+	for ( --array->array_count; index < array->array_count; ++index )
+		array->array_elements[index] = array->array_elements[index + 1];
+	if ( array->array_count == array->array_capacity / 10 )
+		array_decrease_capacity(array);
 
-    return 0; // Removed
+	return 0; // Removed
 }
--- Actividades/2-memcompartida/pthreads/array1/main.c
+++ Actividades/2-memcompartida/pthreads/array1/main.c
@@ -7,37 +7,32 @@
 #define MIN_CNT 10
 #define MAX_CNT 20
 
-void print_array(const char* name, const array_t* array)
+void print_array(const char* name, const array_t array)
 {
-    printf("%s =", name);
-    for ( size_t index = 0; index < array_get_count(array); ++index )
-        printf(" %zu", (size_t)array->array_elements[index]);
-    putchar('\n');
-    fflush(stdout);
+	printf("%s =", name);
+	for ( size_t index = 0; index < array_get_count(array); ++index )
+		printf(" %zu", (size_t)array_get_element(array, index));
+	putchar('\n');
+	fflush(stdout);
 }
 
 int main()
 {
-    array_t* array1 = (array_t*)malloc (MAX_CNT * sizeof(array_t) );// = array_create(array1, MAX_CNT);
-    array_t* array2 = (array_t*)malloc (MAX_CNT * sizeof(array_t) );// = array_create(array2, MAX_CNT);
-    array1 = array_create(array1, MAX_CNT);
-    array2 = array_create(array2, MAX_CNT);
+	array_t array1 = array_create(MAX_CNT);
+	array_t array2 = array_create(MAX_CNT);
 
-    srand( (unsigned int)((unsigned long)time(NULL) + clock()) );
-    for ( size_t index = 0, count = MIN_CNT + rand() % MAX_CNT; index < count; ++index )
-    {
-        array_append( array1, (void*)(10 + (size_t)rand() % 90) );
-        array_append( array2, (void*)(100 + (size_t)rand() % 900) );
-    }
+	srand( (unsigned int)((unsigned long)time(NULL) + clock()) );
+	for ( size_t index = 0, count = MIN_CNT + rand() % MAX_CNT; index < count; ++index )
+	{
+		array_append( array1, (void*)(10 + (size_t)rand() % 90) );
+		array_append( array2, (void*)(100 + (size_t)rand() % 900) );
+	}
 
-    print_array("array1", array1);
-    print_array("array2", array2);
+	print_array("array1", array1);
+	print_array("array2", array2);
 
-    array_destroy(array1);
-    array_destroy(array2);
+	array_destroy(array1);
+	array_destroy(array2);
 
-    free(array1);
-    free(array2);
-
-    return 0;
+	return 0;
 }
