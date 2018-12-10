bf653f46a772ea0241e9d491dc5cdb9f04779236
Lucia Elizondo Sancho <b72694@ecci.ucr.ac.cr@ECCI-lbl-104-13.ecci.ucr.ac.cr>
2018-12-10 14:12:31 -0600

    Arreglo mutex threadsafe

--- Actividades/2-memcompartida/pthreads/array1_thrsafe_mutex/array.c
+++ Actividades/2-memcompartida/pthreads/array1_thrsafe_mutex/array.c
@@ -1,126 +1,121 @@
 #include <assert.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
 #include "array.h"
 
-array_t* array_create(array_t* array, size_t capacity)
+
+array_t array_create(size_t capacity)
 {
 	assert(capacity);
-    array->array_capacity = capacity;
-    array->array_count = 0;
-    array->array_elements = (void**)malloc( capacity * sizeof(void*) );
-    pthread_mutex_init(&array->lock, NULL);
-    return array;
+	array_t temp = (array_t)malloc(sizeof(array_t)); 
+	temp->array_capacity = capacity;
+	temp->array_count = 0;
+	temp->array_elements = (void**)malloc( capacity * sizeof(void*) );
+	pthread_mutex_init(&temp->lock, NULL);
+	return temp; 
 }
 
-void array_destroy(array_t* array)
+void array_destroy(array_t array)
 {
-    free(array->array_elements);
-    pthread_mutex_destroy(&array->lock);
+	pthread_mutex_destroy(&array->lock);
+	free(array->array_elements);
+	free(array);
 }
 
-int array_increase_capacity(array_t* array)
+int array_increase_capacity(array_t array)
 {
-    pthread_mutex_lock(&array->lock);
-    size_t new_capacity = 10 * array->array_capacity;
-    void** new_elements = (void**)realloc( array->array_elements, new_capacity * sizeof(void*) );
-    if ( new_elements == NULL )
-    {
-        pthread_mutex_unlock(&array->lock);
-        return -1;
-    }
+	pthread_mutex_lock(&array->lock);
+	size_t new_capacity = 10 * array->array_capacity;
+	void** new_elements = (void**)realloc( &array->array_elements, new_capacity * sizeof(void*) );
+	if ( new_elements == NULL )
+	{
+		pthread_mutex_unlock(&array->lock);
+		return -1;
+	}
+	
+	array->array_capacity = new_capacity;
+	array->array_elements = new_elements;
+	pthread_mutex_unlock(&array->lock);
 
-    array->array_capacity = new_capacity;
-    array->array_elements = new_elements;
-    pthread_mutex_unlock(&array->lock);
-    return 0; // Success
+	return 0; // Success
 }
 
-int array_decrease_capacity(array_t* array)
+int array_decrease_capacity(array_t array)
 {
-    pthread_mutex_lock(&array->lock);
-    size_t new_capacity = array->array_capacity / 10;
-    if ( new_capacity < 10 )
-    {
-        pthread_mutex_unlock(&array->lock);
-        return 0;
-    }
-
-    void** new_elements = (void**)realloc( array->array_elements, new_capacity * sizeof(void*) );
-    if ( new_elements == NULL )
-    {
-        pthread_mutex_unlock(&array->lock);
-        return -1;
-    }
-
-    array->array_capacity = new_capacity;
-    array->array_elements = new_elements;
-    pthread_mutex_unlock(&array->lock);
-    return 0; // Success
+	pthread_mutex_lock(&array->lock);
+	size_t new_capacity = array->array_capacity / 10;
+	if ( new_capacity < 10 )
+	{
+		pthread_mutex_lock(&array->lock);
+		return 0;
+	}
+	void** new_elements = (void**)realloc( &array->array_elements, new_capacity * sizeof(void*) );
+	if ( new_elements == NULL )
+	{
+		pthread_mutex_unlock(&array->lock);
+		return -1;
+	}
+	
+	array->array_capacity = new_capacity;
+	array->array_elements = new_elements;
+	pthread_mutex_unlock(&array->lock);
+	return 0; // Success
 }
 
-size_t array_get_count(array_t* array)
+size_t array_get_count(const array_t array)
 {
-    pthread_mutex_lock(&array->lock);
-    size_t count = array->array_count;
-    pthread_mutex_unlock(&array->lock);
-    return count;
+	size_t count = 0;
+	pthread_mutex_lock(&array->lock);
+	count = array->array_count;
+	pthread_mutex_unlock(&array->lock);
+	return count; 
 }
 
-void* array_get_element(array_t* array, size_t index)
+void* array_get_element(array_t array, size_t index)
 {
-    assert( index < array_get_count(array) );
-    pthread_mutex_lock(&array->lock);
-    void* element = array->array_elements[index];
-    pthread_mutex_unlock(&array->lock);
-    return element;
+	assert( index < array_get_count(array) );
+	pthread_mutex_lock(&array->lock);
+	void* element = array->array_elements[index];
+	pthread_mutex_unlock(&array->lock);
+	return element;
 }
 
-int array_append(array_t* array, void* element)
+int array_append(array_t array, void* element)
 {
-    pthread_mutex_lock(&array->lock);
-    if ( array->array_count == array->array_capacity )
-        if ( ! array_increase_capacity(array) )
-        {
-            pthread_mutex_unlock(&array->lock);
-            return -1;
-        }
+	if ( array->array_count == array->array_capacity )
+		if ( ! array_increase_capacity(array) )
+			return -1;
 
-    array->array_elements[array->array_count] = element;
-    ++array->array_count;
-    pthread_mutex_unlock(&array->lock);
-    return 0; // Success
+	pthread_mutex_lock(&array->lock);
+	array->array_elements[array->array_count++] = element;
+	pthread_mutex_unlock(&array->lock);
+	return 0; // Success
 }
 
-size_t array_find_first( array_t* array, const void* element, size_t start_pos)
+size_t array_find_first(const array_t array, const void* element, size_t start_pos)
 {
-    pthread_mutex_lock(&array->lock);
-    for ( size_t index = start_pos; index < array->array_count; ++index )
-        if ( array->array_elements[index] == element )
-        {
-            pthread_mutex_unlock(&array->lock);
-            return index;
-        }
-    pthread_mutex_unlock(&array->lock);
-    return array_not_found;
+	pthread_mutex_lock(&array->lock);
+	for ( size_t index = start_pos; index < array->array_count; ++index )
+		if ( array->array_elements[index] == element )
+			return index;
+	pthread_mutex_unlock(&array->lock);
+
+	return array_not_found;
 }
 
-int array_remove_first(array_t* array, const void* element, size_t start_pos)
+int array_remove_first(array_t array, const void* element, size_t start_pos)
 {
-    pthread_mutex_lock(&array->lock);
-    size_t index = array_find_first(array, element, start_pos);
-    if ( index == array_not_found )
-    {
-        pthread_mutex_unlock(&array->lock);
-        return -1;
-    }
+	size_t index = array_find_first(array, element, start_pos);
+	if ( index == array_not_found )
+		return -1;
 
-    for ( --array->array_count; index < array->array_count; ++index )
-        array->array_elements[index] = array->array_elements[index + 1];
-    if ( array->array_count == array->array_capacity / 10 )
-        array_decrease_capacity(array);
-    pthread_mutex_unlock(&array->lock);
-    return 0; // Removed
+	pthread_mutex_lock(&array->lock);
+	for ( --array->array_count; index < array->array_count; ++index )
+		array->array_elements[index] = array->array_elements[index + 1];
+	pthread_mutex_unlock(&array->lock);
+	if ( array->array_count == array->array_capacity / 10 )
+		array_decrease_capacity(array);
+
+	return 0; // Removed
 }
--- Actividades/2-memcompartida/pthreads/array1_thrsafe_mutex/array.h
+++ Actividades/2-memcompartida/pthreads/array1_thrsafe_mutex/array.h
@@ -6,20 +6,22 @@
 
 typedef struct
 {
- void** array_elements;
- size_t array_capacity;
- size_t array_count;
- pthread_mutex_t lock;
-}array_t;
+	size_t array_count; 
+	size_t array_capacity;
+	void** array_elements;  
+	pthread_mutex_t lock;
+}array_struct; 
+
+typedef array_struct* array_t; 
 
 static const size_t array_not_found = (size_t)-1;
 
-array_t *array_create(array_t* array, size_t capacity);
-void array_destroy(array_t* array);
-int array_append(array_t* array, void* element);
-size_t array_get_count(array_t *array);
-void* array_get_element(array_t* array, size_t index);
-size_t array_find_first( array_t* array, const void* element, size_t start_pos);
-int array_remove_first(array_t* array, const void* element, size_t start_pos);
+array_t array_create(size_t capacity);
+void array_destroy(array_t array);
+int array_append(array_t array, void* element);
+size_t array_get_count(const array_t array);
+void* array_get_element(array_t array, size_t index);
+size_t array_find_first(const array_t array, const void* element, size_t start_pos);
+int array_remove_first(array_t array, const void* element, size_t start_pos);
 
 #endif // ARRAY_H
