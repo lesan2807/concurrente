b280f5e4c180c7788b038599d5d05c5910d5e45a
Lucia <lesan2807@gmail.com>
2018-12-11 13:56:25 -0600

    done activity hybrid_distr_arg

--- Actividades/3-memdistribuida/mpi/hybrid_distr_arg/hello.c
+++ Actividades/3-memdistribuida/mpi/hybrid_distr_arg/hello.c
@@ -23,8 +23,6 @@ int main(int argc, char* argv[])
 	
 	size_t a = strtoull(argv[1], NULL, 10); 
 	size_t b = strtoull(argv[2], NULL, 10); 
-	size_t c = (b-a) / number_threads; 
-	size_t r = (b-a) % number_threads; 
 
 	int my_rank = -1; 
 	int process_count = -1;
@@ -36,18 +34,23 @@ int main(int argc, char* argv[])
 	MPI_Comm_size(MPI_COMM_WORLD, &process_count);
 	MPI_Get_processor_name(hostname, &len_hostname); 
 	
+	size_t c = (b-a) / process_count;
+	size_t r = (b-a) % process_count;
+	
 	size_t my_start = start(a, c, my_rank, r);
 	size_t my_finish = finish(a, c, my_rank, r);
 	
-	printf("%s: range [%zu , %zu[ size %zu\n", hostname, my_start , my_finish, my_finish-my_start);
-	#pragma omp parallel for num_threads(number_threads) default(none) shared (a, c, r, hostname, number_threads)
-	for (size_t index = 0; index < number_threads; ++index)
-	{
-		size_t inicio = start(a, c, omp_get_thread_num(), r);
-		size_t final =  finish(a, c, omp_get_thread_num(), r);
-		printf("\t %s:%d: range [%zu , %zu[ size %zu\n", hostname, omp_get_thread_num(), inicio, final, final-inicio); 
-	}
+	size_t c1 = (my_finish-my_start) / number_threads;
+	size_t r1 = (my_finish-my_start) % number_threads;
 	
+	printf("%s:%d range [%zu , %zu[ size %zu\n", hostname, my_rank, my_start , my_finish, my_finish-my_start);
+	#pragma omp parallel num_threads(number_threads) default(none) shared(my_rank, hostname, c1, r1, a, my_start)
+	{ 
+		size_t inicio = start(my_start, c1, omp_get_thread_num(), r1);
+		size_t final = finish(my_start, c1, omp_get_thread_num(), r1);  
+		printf("\t%s:%d.%d: range [%zu , %zu[ size %zu\n", hostname, my_rank, omp_get_thread_num(), inicio, final, final-inicio);
+	}
+
 	MPI_Finalize(); 
 	return 0; 
 }
