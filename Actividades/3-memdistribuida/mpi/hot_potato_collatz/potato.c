857964d7aa31bcb4a8b91ee97cd2dd6cb78c7bbb
Lucia <lesan2807@gmail.com>
2018-12-13 18:00:09 -0600

    Done activity hot potato collatz

---
+++ Actividades/3-memdistribuida/mpi/hot_potato_collatz/potato.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char** argv)
+{
+	MPI_Init( &argc, &argv );
+  	
+	int my_rank = -1; 
+	int process_count = -1;
+	long long potato = 0; 
+	int initial_process = 0;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &process_count);
+
+	if(argc > 2 )
+	{
+		potato = strtoull(argv[1], NULL, 10);
+		initial_process = atoi(argv[2]);       
+		if((long long)potato < 0 || initial_process < 0)
+			return printf("%s\n", "Must be positive"), 2; 
+	}
+	else 
+		return printf("%s\n", "No potato given nor initial process"), 1;
+		
+	long long my_potato = potato;
+	
+	int prev = (my_rank - 1 + process_count) % process_count; 
+	int next = (my_rank + 1) % process_count; 
+	
+	int total = process_count; 
+	
+	int alive = 1; 
+
+	if(my_rank == initial_process)
+	{
+		MPI_Send(&my_potato, 1, MPI_LONG_LONG_INT, next, 0, MPI_COMM_WORLD); 
+		MPI_Send(&total, 1, MPI_INT, next, 0, MPI_COMM_WORLD); 
+	}
+	
+			
+	while( potato > 1 && total > 1)
+	{
+		MPI_Recv(&my_potato, 1, MPI_LONG_LONG_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+		MPI_Recv(&total, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+		if(alive != 0)
+		{
+			if(my_potato % 2 == 0)
+				my_potato /= 2;
+			else 
+				my_potato = 3*my_potato + 1; 
+		}
+		printf("papa %lld on process %d\n", my_potato, my_rank);
+		if( my_potato == 1)
+		{
+			--total;
+			my_potato = potato; 
+			alive = 0;
+		}
+		MPI_Send(&my_potato, 1, MPI_LONG_LONG_INT, next, 0, MPI_COMM_WORLD);
+		MPI_Send(&total, 1, MPI_INT, next, 0, MPI_COMM_WORLD); 
+		if(total == 1 && alive != 0)
+			printf("I won! %d\n", my_rank);
+	}
+	
+	MPI_Finalize(); 
+
+	return 0;
+}
